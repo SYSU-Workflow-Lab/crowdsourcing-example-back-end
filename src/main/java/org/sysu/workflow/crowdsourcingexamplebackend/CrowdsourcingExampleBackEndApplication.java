ecc09fa46b709f12cc4f401ee820a28b03687390
SkyeBeFreeman <928016560@qq.com>
2018-09-28 20:10:51 +0800

    first commit

---
+++ src/main/java/org/sysu/workflow/crowdsourcingexamplebackend/CrowdsourcingExampleBackEndApplication.java
@@ -0,0 +1,12 @@
+package org.sysu.workflow.crowdsourcingexamplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsourcingExampleBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdsourcingExampleBackEndApplication.class, args);
+    }
+}
---
+++ src/test/java/org/sysu/workflow/crowdsourcingexamplebackend/CrowdsourcingExampleBackEndApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sysu.workflow.crowdsourcingexamplebackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrowdsourcingExampleBackEndApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
